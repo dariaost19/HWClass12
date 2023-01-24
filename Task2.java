eec3134fc49fa6431b4a9baa45d23768f4a80b63
dariaost19 <dariaost19@gmail.com>
2023-01-23 19:14:36 -0500

    updatedHW10

--- Task2.java
+++ Task2.java
@@ -1,22 +1,18 @@
 package homework10;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Task2 {
+public class Task002 {
     public static void main(String[] args) {
-        //2) Create a String and print it in reverse order (Sunday → yadnuS).
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter word");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Please enter a word");
         String word = scan.next();
-        StringBuilder reverseString = new StringBuilder(word);
-        reverseString.reverse();
-        String reverse = reverseString.toString();
+        String reverse = "";
 
-        System.out.print(reverse + " ");
+        for(int i =0;i<word.length(); i++) {
+            reverse = word.charAt(i)+reverse;
+        }
 
-
+        System.out.println(reverse);
     }
 }
-
-
